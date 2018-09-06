225a4611c80061aa1e61fd01c2b9ff936510d8b6
quasim <nicomaxa@gmail.com>
2018-09-06 16:35:16 +0200

    Chap03 The app state

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ new Vue({
 
     template: `<div id="#app">
             Hello World!
+            {{ worldRatio}}
         </div>`,
+
+    data: state,
+
+    // Window resize handling
+    window: addEventListener('resize', () => {state.worldRatio = getWorldRatio()}),
+
+    mounted(){
+        console.log(this.$data === state)// true or false
+    },
 
 })
